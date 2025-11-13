6da36962dcbcf7150b4831d16f70040cc945a1aa
Nadia Torres <nadia.torres@tecsup.edu.pe>
2025-11-13 11:48:18 -0500

    Fix: Apply .gitignore and remove tracked build files

--- Lab13Web-NadiaTorres.API/obj/Debug/net9.0/Lab13Web-NadiaTorres.API.AssemblyInfo.cs
+++ Lab13Web-NadiaTorres.API/obj/Debug/net9.0/Lab13Web-NadiaTorres.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab13Web-NadiaTorres.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+967a996a8bedbc6381ce5e744614fa4f8303a2d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab13Web-NadiaTorres.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab13Web-NadiaTorres.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
